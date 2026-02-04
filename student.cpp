0053f11a2f9428578f7c5ab13620ebcec7f3e4a8
anjali jha <ajha42413@gmail.com>
2026-02-05 02:52:15 +0530

    Create student.cpp

---
+++ student.cpp
@@ -0,0 +1,33 @@
+#include"student.h"
+#include<iostream>
+using namespace std;
+//initialise
+Student::Student()
+{
+  //to_avoid_garbage_value
+rollnumber=0;
+  name="";
+  branch="";
+  year=0;
+}
+//take_input
+void Student::inputdetails()
+{
+  cout<<"enter roll number: ";
+cin>>rollnumber;
+cout<<"enter name:";
+cin>>name;
+cout<<"enter branch:";
+cin>>branch;
+cout<<"enter year:";
+cin>>year;
+}
+//display_output
+void Student::displaydetails()
+{
+  cout<<"\nstudent details:\n";
+cout<<"roll number = "<<rollnumber<<endl;
+cout<<"name="<<name<<endl;
+cout<<"branch="<<branch<<endl;
+cout<<"year="<<year<<endl;
+}
